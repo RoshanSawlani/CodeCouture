24028abbf62af41b31cbdca09ebfa63cc0c01f62
RoshanSawlani <roshansawlani@gmail.com>
2024-05-20 19:44:44 +0530

    Designing the Tshirts page for CodeCouture

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,20 +5,20 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2'>
+    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md'>
       <div className="logo mx-5">
-          <Image src="/logo.png" width={50} height={40} alt="logo"/>
+          <Link href={'/'} legacyBehavior><a><Image src="/logo.png" width={50} height={40} alt="logo"/></a></Link>
         </div>      
       <div className="nav">
-        <ul className='flex items-center space-x-2 font-bold md:text-xl'>
-          <Link href={'/'} legacyBehavior><a><li>Tshirts</li></a></Link>
-          <Link href={'/'} legacyBehavior><a><li>Hoodies</li></a></Link>
-          <Link href={'/'} legacyBehavior><a><li>Stickers</li></a></Link>
-          <Link href={'/'} legacyBehavior><a><li>Mugs</li></a></Link>
+        <ul className='flex items-center space-x-4 font-bold md:text-md'>
+          <Link href={'/tshirts'} legacyBehavior><a><li>Tshirts</li></a></Link>
+          <Link href={'/hoodies'} legacyBehavior><a><li>Hoodies</li></a></Link>
+          <Link href={'/stickers'} legacyBehavior><a><li>Stickers</li></a></Link>
+          <Link href={'/mugs'} legacyBehavior><a><li>Mugs</li></a></Link>
         </ul>
       </div>
       <div className="cart absolute right-0 top-4 mx-5">
-        <AiOutlineShoppingCart className='text-xl md:text-3xl'/>
+        <AiOutlineShoppingCart className='text-xl md:text-2xl'/>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>      
-      <Navbar/>
+      
       <div className="">
         <img src="/homepage.jpg" alt="homepage" />
       </div>
@@ -103,8 +101,6 @@ export default function Home() {
     
   </div>
 </section>
-
-      <Footer/>
     </div>
   );
 }
