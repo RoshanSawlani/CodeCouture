fd26b7037edb243466693cf0a4b7c286c7acb9e0
RoshanSawlani <roshansawlani@gmail.com>
2024-05-29 22:15:52 +0530

    Displaying color and size variants on tshirt page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -69,9 +69,9 @@ const Post = ({addToCart}) => {
                         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                             <div className="flex">
                                 <span className="mr-3">Color</span>
-                                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none" />
-                                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none" />
-                                <button className="border-2 border-gray-300 ml-1 bg-pink-500 rounded-full w-6 h-6 focus:outline-none" />
+                                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
+                                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                                <button className="border-2 border-gray-300 ml-1 bg-pink-500 rounded-full w-6 h-6 focus:outline-none"></button>
                             </div>
                             <div className="flex ml-6 items-center">
                                 <span className="mr-3">Size</span>
