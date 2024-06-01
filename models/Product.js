46c26e0170f2eba911e80cf3f75eece5061ead7f
RoshanSawlani <roshansawlani@gmail.com>
2024-06-01 14:47:51 +0530

    Creating Signup Page for CodeCouture

--- models/Product.js
+++ models/Product.js
@@ -38,5 +38,5 @@ const ProductSchema = new mongoose.Schema({
     }
 },{timestamps:true})
 
-mongoose.models ={}
-export default mongoose.model("Product",ProductSchema)
+
+export default mongoose.models.Product || mongoose.model("Product",ProductSchema)
--- pages/signup.js
+++ pages/signup.js
@@ -1,52 +1,86 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
 
 const Signup = () => {
-  return (
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
 
-    <div>
-      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-10 w-auto" src="/logo.png" alt="Your Company" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up to your account</h2>
+  const handleChange = (e) => {
+    if (e.target.name === 'name') {
+      setName(e.target.value)
+    }
+    else if (e.target.name === 'email') {
+      setEmail(e.target.value)
+    }
+    else if (e.target.name === 'password') {
+      setPassword(e.target.value)
+    }
+  }
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+      const data = { name, email, password }
+      let res = await fetch("http://localhost:3004/api/signup", {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      let response = await res.json();
+      console.log(response)
+
+      setEmail("")
+      setName("")
+      setPassword("")
+      toast.success("Your account has been created")
+    }
+    return (
+      <div>
+        <Toaster/>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img className="mx-auto h-10 w-auto" src="/logo.png" alt="Your Company" />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up to your account</h2>
+          </div>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form onSubmit={handleSubmit} className="space-y-6" method="POST">
+              <div>
+                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                <div className="mt-2">
+                  <input value={name} onChange={handleChange} id="name" name="name" type="text" autoComplete="name" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <div className="mt-2">
+                  <input value={email} onChange={handleChange} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+
+                </div>
+                <div className="mt-2">
+                  <input value={password} onChange={handleChange} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+              <div>
+                <button type="submit" className="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Sign up</button>
+              </div>
+            </form>
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Not a member?
+              <Link href={'/login'} legacyBehavior><a href="#" className="font-semibold leading-6 text-pink-600 hover:text-pink-500">Login</a></Link>
+            </p>
+          </div>
         </div>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
-              <div className="mt-2">
-                <input id="name" name="name" type="text" autoComplete="name" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-              <div className="mt-2">
-                <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                
-              </div>
-              <div className="mt-2">
-                <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-            <div>
-              <button type="submit" className="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Sign up</button>
-            </div>
-          </form>
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?
-            <Link href={'/login'} legacyBehavior><a href="#" className="font-semibold leading-6 text-pink-600 hover:text-pink-500">Login</a></Link>
-          </p>
-        </div>
+
       </div>
 
-    </div>
-
-  )
-}
-
-export default Signup
+    )
+  }
+  export default Signup
