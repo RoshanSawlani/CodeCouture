dc0bea2071b7bc527be85128f7bac9c431f13c0d
RoshanSawlani <roshansawlani@gmail.com>
2024-06-06 14:39:05 +0530

    Adding Payment Gateway to CodeCouture - pending

---
+++ pages/api/posttransaction.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  // Update status into orders table after checking the transaction status
+  // initiate shipping
+  // Redirect user to the order confirmation page
+    res.status(200).json({body:req.body});
+  }
+  
