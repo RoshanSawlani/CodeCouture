5497c8162f44c9fe33f160a8e0dbe5732f074c31
RoshanSawlani <roshansawlani@gmail.com>
2024-06-13 12:58:24 +0530

    Showing Sidebar only for few pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useRef, useState } from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect, useRef, useState } from 'react'
 import { AiOutlineShoppingCart, AiFillCloseCircle, AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 import { BsFillBagCheckFill } from "react-icons/bs";
 import { MdAccountCircle } from "react-icons/md";
@@ -8,20 +9,42 @@ import { MdAccountCircle } from "react-icons/md";
 
 const Navbar = ({logout, user, cart, addToCart, removeFromCart, clearCart, subTotal }) => {
   const [dropdown,setDropdown] = useState(false)
-  // console.log(cart,addToCart,removeFromCart,clearCart,subTotal)
+  const [sidebar,setSidebar] = useState(false)
+  const router = useRouter
+
+  useEffect(()=>{
+    Object.keys(cart).length !== 0 && setSidebar(true)
+    let exempted = ['/checkout','/order','/orders','/myaccount']
+    if(exempted.includes(router.pathname)){
+      setSidebar(false)
+    }
+  },[])
+  
   const toggleCart = () => {
-    if (ref.current.classList.contains('translate-x-full')) {
+    setSidebar(!sidebar)
+    /* if (ref.current.classList.contains('translate-x-full')) {
       ref.current.classList.remove('translate-x-full');
       ref.current.classList.add('translate-x-0')
     }
     else if (!ref.current.classList.contains('translate-x-full')) {
       ref.current.classList.remove('translate-x-0')
       ref.current.classList.add('translate-x-full')
-    }
+    } */
   }
   const ref = useRef()
   return (
-    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md sticky top-0 bg-white z-10'>
+    <>
+    {!sidebar && <span onMouseOver={()=>{setDropdown(true)}} onMouseLeave={()=>{setDropdown(false)}} className="absolute right-9 top-4 z-30 cursor-pointer">
+      {dropdown && <div className="absolute right-5 bg-white shadow-lg border top-5 py-4 rounded-md px-5 w-32 z-30">
+        <ul>
+          <Link href={'/myaccount'} legacyBehavior><a><li className='py-1 hover:text-pink-700 text-sm font-bold'>My Account</li></a></Link>
+          <Link href={'/orders'} legacyBehavior><a><li className='py-1 hover:text-pink-700 text-sm font-bold'>Orders</li></a></Link>
+          <li onClick={logout} className='py-1 hover:text-pink-700 text-sm font-bold'>Logout</li>
+        </ul>
+      </div>}
+      {user.value && <MdAccountCircle className='text-xl md:text-2xl mx-2'/>}
+      </span>}
+    <div className={`flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md sticky top-0 bg-white z-10 ${!sidebar && 'overflow-hidden'}`}>
       <div className="logo mr-auto md:mx-5">
         <Link href={'/'} legacyBehavior><a><Image src="/logo.png" width={50} height={40} alt="logo" /></a></Link>
       </div>
@@ -34,21 +57,14 @@ const Navbar = ({logout, user, cart, addToCart, removeFromCart, clearCart, subTo
         </ul>
       </div>
       <div className="cart items-center cursor-pointer absolute right-0 top-4 mx-5 flex">
-        <span onMouseOver={()=>{setDropdown(true)}} onMouseLeave={()=>{setDropdown(false)}}>
-      {dropdown && <div onMouseOver={()=>{setDropdown(true)}} onMouseLeave={()=>{setDropdown(false)}} className="absolute right-8 bg-white shadow-lg border top-6 py-4 rounded-md px-5 w-32">
-        <ul>
-          <Link href={'/myaccount'} legacyBehavior><a><li className='py-1 hover:text-pink-700 text-sm font-bold'>My Account</li></a></Link>
-          <Link href={'/orders'} legacyBehavior><a><li className='py-1 hover:text-pink-700 text-sm font-bold'>Orders</li></a></Link>
-          <li onClick={logout} className='py-1 hover:text-pink-700 text-sm font-bold'>Logout</li>
-        </ul>
-      </div>}
-      {user.value && <MdAccountCircle className='text-xl md:text-2xl mx-2'/>}
-      </span>
+        
       {!user.value && <Link href={'/login'} legacyBehavior><a><button className='bg-pink-600 px-2 py-1 rounded-md text-sm text-white mx-2'>Login</button></a></Link>}
+
+      
         <AiOutlineShoppingCart onClick={toggleCart}  className='text-xl md:text-2xl' />
       </div>
       
-      <div ref={ref} className={`w-72 h-[100vh] sideCart overflow-y-scroll absolute top-0 right-0 bg-pink-100 px-8 py-10 transform transition-transform ${Object.keys(cart).length !== 0 ? 'translate-x-0' : 'translate-x-full'}`}>
+      <div ref={ref} className={`w-72 h-[100vh] sideCart overflow-y-scroll absolute top-0 bg-pink-100 px-8 py-10  transition-all ${sidebar ? 'right-0' : '-right-96'}`}>
         <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
         <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"><AiFillCloseCircle /></span>
         <ol className='list-decimal font-semibold'>
@@ -69,6 +85,7 @@ const Navbar = ({logout, user, cart, addToCart, removeFromCart, clearCart, subTo
         </div>
       </div>
     </div>
+    </>
   )
 }
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import mongoose from 'mongoose'
 import Product from '@/models/Product'
 import toast, { Toaster } from 'react-hot-toast';
@@ -9,6 +9,13 @@ const Post = ({ addToCart, buyNow, product, variants }) => {
     const { slug } = router.query
     const [pin, setPin] = useState()
     const [service, setService] = useState()
+    const [color, setColor] = useState(product.color)
+    const [size, setSize] = useState(product.size)
+
+    useEffect(()=>{
+        setColor(product.color)
+        setSize(product.size)
+    },[router.query])
 
     const checkServiceability = async () => {
         let pins = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pincode`)
@@ -26,12 +33,12 @@ const Post = ({ addToCart, buyNow, product, variants }) => {
     const onChangePin = (e) => {
         setPin(e.target.value)
     }
-    const [color, setColor] = useState(product.color)
-    const [size, setSize] = useState(product.size)
+
 
     const refreshVariant = (newsize, newcolor) => {
         let url = `${process.env.NEXT_PUBLIC_HOST}/product/${variants[newcolor][newsize]['slug']}`
         window.location = url
+        router.push(url)
     }
 
 
